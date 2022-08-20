9cd5aa1cabc1efb54bc1bc97aa2bbc4b0c0ad459
VladimirR <Vova.ru0007@yandex.ru>
2022-08-20 11:49:42 +0500

    zadacha36

---
+++ Zad36/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях(по индексу считаем).
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+int[] arr = new int[4];
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Random rnd = new Random();
+        int value = rnd.Next(50);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void SumUnevenElements(int[] array)
+{
+    int sumNumber = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sumNumber += arr[i];
+        }
+
+    }
+    Console.Write($"Сумма  нечетных  чисел: {sumNumber}");
+}
+RandomArray(arr);
+SumUnevenElements(arr);
+
